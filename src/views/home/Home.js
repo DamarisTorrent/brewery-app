698454e71b15bbf578f193a5c630aeb371c93c8e
Damaris Torrent <damaristorrent@gmail.com>
2023-02-21 08:47:57 -0500

    Added pigeon map and marker

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,7 +1,39 @@
+import { Map, Marker } from "pigeon-maps"
+import { useState, useEffect } from 'react'
 
 function Home() {
+  
+  const [anchor, setAnchor] = useState([])
+  const [latLng, setLatLng] = useState()
+  const [name, setName] = useState()
+
+  console.log(name)
+  console.log(latLng)
   return (
-    <div>Home</div>
+    <div>
+
+<Map 
+      height={300} 
+      defaultCenter={[39.8283, -98.5795]} 
+      defaultZoom={4}
+      onClick={({latLng}) => {
+        setLatLng(latLng)
+        setAnchor(latLng)
+        setName('Test name onclick')
+      }}>
+
+      <Marker 
+        width={25} 
+        anchor={latLng}>
+      </Marker>
+
+      {/* <Card>
+        name = {name}
+      </Card> */}
+      
+    </Map>
+
+    </div>
   )
 }
 
