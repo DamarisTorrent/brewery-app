05b14e870956f234262a70df0bd30caa4dc6fd07
Damaris Torrent <damaristorrent@gmail.com>
2023-02-23 12:12:34 -0500

    Added icons, flex, header logo, like functionality

--- src/views/favorites/Favorites.js
+++ src/views/favorites/Favorites.js
@@ -1,14 +1,40 @@
 import { useContext } from 'react'
 import FavoritesContext from '../../context/FavoritesContext'
+import BrewCard from '../../components/BrewCard'
+import { AppBar } from '../../components/AppBar'
 
 function Favorites() {
 
   const {favoriteList} = useContext(FavoritesContext)
- console.log(favoriteList)
+  console.log(favoriteList)
+  
   return (
-    <div>
-      <h1> this is the favorites page</h1>
-    </div> 
+    
+      
+      <div>
+       
+
+      {favoriteList.map((element) => {
+        return (
+          <div >
+            <BrewCard
+              name={element.name}
+              brewery_type={element.brewery_type}
+              street={element.street}
+              city={element.city}
+              state={element.state}
+              postal_code={element.postal_code}
+              country={element.country}
+              phone={element.phone}
+              website_url={element.website_url}
+            />
+          </div>
+          )
+          })}
+    </div>
+   
+
+    
 
     
   )
